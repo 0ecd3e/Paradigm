19786cb9cb5568e38743c0fd7314e052cf55f788
jackwang316 <52519516+jackwang316@users.noreply.github.com>
2019-12-28 17:22:00 -0800

    Added in RSS feed

--- app/src/main/java/com/paradigm/paradigm/NewsFeed/Feed.java
+++ app/src/main/java/com/paradigm/paradigm/NewsFeed/Feed.java
@@ -3,18 +3,25 @@ package com.paradigm.paradigm.NewsFeed;
 import java.io.*;
 import java.net.*;
 import java.util.*;
-import java.sql.Timestamp;
 
 public class Feed {
 
-    private static String getTitle(final String curLine){
+    private Vector<Article> list=new Vector<>();
+
+    private static String getTitle(final String line){
         //Formats article title from RSS.
-        int start = curLine.indexOf("<title>");
-        String temp = curLine.substring(start);
+        int start = line.indexOf("<title>");
+        String temp = line.substring(start);
         temp = temp.replace("<title>", "");
         int end = temp.indexOf("</title>");
-        temp = temp.substring(0, end);
-        return temp;
+        return temp.substring(0, end);
+    }
+
+    private static String getLink(final String line){
+        int start=line.indexOf("<link>");
+        String temp = line.substring(start);
+        int end = temp.indexOf("</link>");
+        return temp.substring(0,end);
     }
 
     private static String getTimeStamp(final String line){
@@ -23,37 +30,45 @@ public class Feed {
         String timeStamp = line.substring(start);
         timeStamp = timeStamp.replace("<pubDate>", "");
         int end = timeStamp.indexOf("</pubDate>");
-        timeStamp = timeStamp.substring(0, end);
-        return timeStamp;
+        return timeStamp.substring(0, end);
     }
 
-    public static String readRSS(final String address){
-        //Establishes connection to RSS and retrieves article infos.
+    public void printList(){
+        for (int i=0; i<list.size(); i++){
+            list.elementAt(i).print();
+        }
+    }
+
+    public void setFeed(final String address){
+        //Establishes connection to RSS and retrieves article info.
         try {
             URL newsUrl = new URL(address);
             BufferedReader in = new BufferedReader(new InputStreamReader(newsUrl.openStream()));
             StringBuilder source= new StringBuilder();
             String line;
-
+            Article temp=new Article();
             while ((line=in.readLine())!=null){
                 if(line.contains("<title>")) {
-                    source.append(getTitle(line));
+                    temp.setTitle(getTitle(line));
                 }
                 if(line.contains("<pubDate>")){
-                    source.append("  ").append(getTimeStamp(line)).append("\n");
+                    temp.setTime(getTimeStamp(line));
                 }
+                if(line.contains("<link>")){
+                    temp.setUrl(getLink(line));
+                }
+                list.add(temp);
             }
             in.close();
-            return source.toString();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return "";
     }
 
     public static void main(String[] args){
         String url="https://www.cbc.ca/cmlink/rss-canada";
-        String finalUrl=readRSS(url);
-        System.out.print(finalUrl);
+        Feed f=new Feed();
+        f.setFeed(url);
+        f.printList();
     }
 }
