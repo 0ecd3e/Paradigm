73fa0ac9c52b7236aa039326fa9ffa2ecb3699b6
jackwang316 <52519516+jackwang316@users.noreply.github.com>
2019-12-29 17:04:32 -0800

    Added in RSS feed

---
+++ app/src/main/java/com/paradigm/paradigm/NewsFeed/Article.java
@@ -0,0 +1,38 @@
+package com.paradigm.paradigm.NewsFeed;
+
+public class Article {
+    private String title;
+    private String time;
+    private String url;
+
+
+    public void setTitle(final String name){
+        title=name;
+    }
+
+    public void setTime(final String timeStamp){
+        time=timeStamp;
+    }
+
+    public void setUrl(final String link){
+        url=link;
+    }
+
+    public final String getTitle(){ return  title; }
+
+    public final String getTime(){ return time;}
+
+    public final String getUrl(){ return url; }
+
+    public void print(){
+        System.out.print(title);
+        System.out.print(time);
+        System.out.print(url+"\n");
+    }
+
+    public static void main(String[] args){
+
+    }
+
+
+}
